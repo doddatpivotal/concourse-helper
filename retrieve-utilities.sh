1c26d6540b768e0394fe67ead859670021a3e3e6
Dodd Pfeffer <dpfeffer@pivotal.io>
2021-02-12 13:07:06 -0500

    Bumps kp to 1.1.1 and pivnet to 3.0.0

--- retrieve-utilities.sh
+++ retrieve-utilities.sh
@@ -15,19 +15,13 @@ chmod +x ./kapp-linux-amd64
 mv kapp-linux-amd64 kapp
 
 rm pivnet
-curl -LO https://github.com/pivotal-cf/pivnet-cli/releases/download/v2.0.1/pivnet-darwin-amd64-2.0.1
-chmod +x ./pivnet-darwin-amd64-2.0.1
-mv pivnet-darwin-amd64-2.0.1 pivnet
+curl -LO https://github.com/pivotal-cf/pivnet-cli/releases/download/v3.0.0/pivnet-darwin-amd64-3.0.0
+chmod +x ./pivnet-darwin-amd64-3.0.0
+mv pivnet-darwin-amd64-3.0.0 pivnet
 
 rm kp
 ./pivnet download-product-files \
   --product-slug='build-service' \
-  --release-version='1.0.3' \
-  --product-file-id=817470 \
-  --config=$PIVNET_CONFIG
- chmod +x ./kp-linux-0.1.3
- mv kp-linux-0.1.3 kp
-
 rm tmc
 curl -LO https://tmc-cli.s3-us-west-2.amazonaws.com/tmc/0.2.0-ba47223d/linux/x64/tmc
 chmod +x ./tmc
